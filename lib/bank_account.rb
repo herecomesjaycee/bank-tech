73df69e7ced5562faa60ad446ced6a695940968d
herecomesjaycee <jaycee.cheong04@gmail.com>
2017-03-09 14:02:33 +0000

    Create and test BankAccount class

---
+++ lib/bank_account.rb
@@ -0,0 +1,15 @@
+class BankAccount
+  attr_reader :balance
+
+  def initialize(balance = 0)
+    @balance = balance
+  end
+
+  def credit(amount)
+    @balance += amount
+  end
+
+  def debit(amount)
+    @balance -= amount
+  end
+end
---
+++ spec/bank_account_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+require_relative '../lib/bank_account'
+
+describe BankAccount do
+  subject(:acc) { described_class.new }
+  
+  describe 'initialise with balance' do
+    it 'will find attribute balance' do
+      expect(acc.balance).to eq 0
+    end
+  end
+
+  describe 'credit with amount' do
+    it 'will add amount to balance' do
+      expect(acc.credit(5.50)).to eq 5.50
+    end
+    end
+
+    describe 'debit with amount' do
+    it 'will remove amount from balance' do
+      acc.credit(5.00)
+      expect(acc.debit(3.50)).to eq 1.50
+    end
+    end
+end
